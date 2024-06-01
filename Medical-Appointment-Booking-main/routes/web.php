0de46fc335a5cef0cb803005219a84058b30a2cf
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-06-01 08:26:38 +0700

    add google login

--- Medical-Appointment-Booking-main/routes/web.php
+++ Medical-Appointment-Booking-main/routes/web.php
@@ -2,11 +2,16 @@
 
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\PostController;
-use App\Http\Controllers\Frontend\ProfileController as FrontendProfileController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\MyAppointmentController;
 use App\Http\Controllers\User\BookingAppointmentController;
+use App\Http\Controllers\User\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User; 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Patient; 
+use Illuminate\Support\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,20 +24,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,"index"]);
-
+Route::get("{id}/specialization-book",[HomeController::class,"specialzationBook"])->name('specialization-book');
+Route::get("/specialization-list",[HomeController::class,"specialzationList"])->name('specialization-list');
 Route::get("{id}/news-detail",[PostController::class,"show"])->name('news-detail');
 Route::get("/doctor-team",[HomeController::class,"doctorTeam"])->name('doctor-team');
+Route::get("/news",[HomeController::class,"newsList"])->name('news');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Login with society ---------------------------------------
+//  By Google 
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name("google.redirect");
+ 
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('google')->user();
+    $user = User::firstOrCreate([
+        'email' => $user->email, 
+    ],[
+        'password' => bcrypt(Str::random(24))
+    ]);
+    $patient = Patient::create([
+        "user_id" => $user->id,
+    ]);
+    $patient->update(["patient_id" => $user->id.$user->phone.$patient->id.Carbon::create()->isoFormat("YYYYMMDDDD")]);
+    Auth::login($user,true);
+    return redirect('/');
+});
+
+// Login with society ---------------------------------------
 
 Route::middleware('auth')->group(function () {
 
+    // My Appointment  
+    Route::put('cancel-appointment',[MyAppointmentController::class,'cancelAppointment'])->name('cancel-appontment');
+    Route::get('my-appointment',[MyAppointmentController::class,"index"])->name("my-appointment");
     // Profile  
-    Route::put("profile/password-update",[FrontendProfileController::class,'passwordUpdate'])->name("profile.password-update");
-    Route::put("profile/update",[FrontendProfileController::class,"profileUpdate"])->name("profile.update"); 
-    Route::get("profile",[FrontendProfileController::class,"index"])->name("profile"); 
+    Route::put("profile/password-update",[ProfileController::class,'passwordUpdate'])->name("profile.password-update");
+    Route::put("profile/update",[ProfileController::class,"profileUpdate"])->name("profile.update"); 
+    Route::get("profile",[ProfileController::class,"index"])->name("profile"); 
 
     // Booking Appoitment
     Route::get("{id}/booking-success",[BookingAppointmentController::class,"bookingSuccess"])->name("booking-success");
